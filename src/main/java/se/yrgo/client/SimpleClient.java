701b6f428d9dc8009a551099a5e810b82b6d3708
Haimen <haimenabbas@gmail.com>
2025-04-22 15:49:22 +0200

    Uppgift 2 + 3 - defined application.xml and SimpleClient base

--- src/main/java/se/yrgo/client/SimpleClient.java
+++ src/main/java/se/yrgo/client/SimpleClient.java
@@ -1,10 +1,24 @@
 package se.yrgo.client;
 
+import java.util.*;
 
+import org.springframework.context.support.*;
+
+import se.yrgo.domain.*;
+import se.yrgo.services.customers.*;
 
 public class SimpleClient {
 
     public static void main(String[] args) {
 
+        ClassPathXmlApplicationContext container = new ClassPathXmlApplicationContext("application.xml");
+
+        CustomerManagementService service = container.getBean(CustomerManagementService.class);
+
+        List<Customer> customers = service.getAllCustomers();
+
+        for (Customer c : customers) {
+            System.out.println(c);
+        }
     }
 }
